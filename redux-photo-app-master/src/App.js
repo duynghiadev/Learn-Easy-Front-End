e415b0b92d30e84dbc3031b0849cc1c064d02951
duynghiadev <duynghia22302@gmail.com>
2023-02-26 03:07:56 +0700

    Gắn API Module vào ReactJS project 🎉

--- redux-photo-app-master/src/App.js
+++ redux-photo-app-master/src/App.js
@@ -1,4 +1,7 @@
+import productApi from "api/productApi";
 import React, { Suspense } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header";
@@ -8,6 +11,26 @@ import NotFound from "./components/NotFound";
 const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          _page: 1,
+          _limit: 10,
+        };
+        const response = await productApi.getAll(params);
+        console.log(response);
+        setProductList(response.data);
+      } catch (error) {
+        console.log("Failed to fetch product list: ", error);
+      }
+    };
+
+    fetchProductList();
+  }, []);
+
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
